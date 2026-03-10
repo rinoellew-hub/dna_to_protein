909f930f59ab23a31188154f7440df83013af9b1
Rinoelle Wisco <rinoellew@gmail.com>
2026-03-10 08:50:01 -0700

    Initial commit

---
+++ src/dna_to_protein.java
@@ -0,0 +1,19 @@
+/**
+ * Program name: dna_to_protein
+ * Author: Rinoelle Wisco
+ * Date: March 9th, 2024
+ * Compiler: JDK 24.0.1
+ * */
+
+import java.util.Scanner;
+import java.util.*;
+
+public class dna_to_protein {
+
+    public static void main (String [] args) {
+
+        
+
+
+    }
+}
