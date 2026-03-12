75ded25a149829b8c4976345536c15a3b48538a4
Rinoelle Wisco <rinoellew@gmail.com>
2026-03-12 13:17:38 -0700

    updated commit.

--- src/dna_seq_gen.java
+++ src/dna_seq_gen.java
@@ -4,27 +4,29 @@
  * Date: March 10th, 2024
  * Compiler: JDK 24.0.1
  * */
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.*;
 import java.util.Random;
 
 public class dna_seq_gen {
 
-    public static void main(String[] args) {
+    public static void getSeq() throws FileNotFoundException {
 
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome user!");
-        System.out.print("Please enter the length that you want your DNA sequence: ");
+        System.out.print("Please enter the length that you want your random DNA non-coding sequence: ");
 
 
         int dna_len = scanner.nextInt();
 
         String [] arr = dnaSeq(dna_len);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+        PrintWriter output = new PrintWriter("dna.txt");
+        for (String s : arr) {
+            output.print(s);
         }
+        output.close();
 
     }
 
--- src/dna_to_protein.java
+++ src/dna_to_protein.java
@@ -7,6 +7,7 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.*;
 
@@ -16,26 +17,76 @@ public class dna_to_protein {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome user!");
-        String myInput = getInputFileName(scanner); // calls method to get user input of file name for their input
+        System.out.println("Welcome user! ");
+        System.out.println("I am a program that can take a random DNA sequence and can transcribe and translate the sequence into a " +
+                "their respective text file.\n");
 
 
-        HashMap <String, String> codonChart = getCodonChart();
+        dna_seq_gen.getSeq();
 
-        // Initialize the array with words from the input file
-        String[] wordArr = array_Initialization(myInput);
+         //Initialize the array with words from the input file
+        char [] sequence = temp_Initialization("dna.txt");
 
+
+        String [] transcription = dna_convertTo_mRNA(sequence);
+        String [] triplet = mRNA_tripletConversion(transcription);
+        String [] translation = mRNA_convertTo_Protein(triplet);
+
+        printFileMRNA(triplet);
+        printFileProtein(translation);
 
     }
 
+    public static void printFileMRNA(String [] str) throws FileNotFoundException {
+
+            PrintWriter output = new PrintWriter("mrna.txt");
+
+            for (String s : str) {
+                    output.print(s + " ");
+            }
+        output.close();
+
+    }
+
+    public static void printFileProtein(String [] str) throws FileNotFoundException {
+        PrintWriter output = new PrintWriter("protein.txt");
+        int index = 0;
+        for (String s : str) {
+
+            if(index < str.length - 1) {
+                output.print(s + "-");
+            } else {
+                output.print(s + " ");
+            }
+
+            index++;
+        }
+        output.close();
+    }
+
+    public static String [] charToString (char [] arr) {
+
+        String [] new_arr = new String [arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            char tempChar = arr[i];
+            String tempStr = tempChar + "";
+
+            new_arr[i] = tempStr;
+        }
+
+        return new_arr;
+    }
+
     /**
-     * Prompts the user for the input file name
+     * Prompts the user for the output file name.
      *
      * @param input the scanner object to read user input
-     * @return the input file name
+     * @return the output file name
      */
-    public static String getInputFileName(Scanner input) {
-        System.out.print("Enter an input file of your given DNA sequence you want to converge: ");
+    public static String getOutputFileName(Scanner input) {
+        System.out.print("Enter an output file name: ");
         return input.nextLine();
     }
 
@@ -193,52 +244,232 @@ public class dna_to_protein {
         return mergeCodonChart(uCodonChart,cCodonChart, aCodonChart, gCodonChart);
     }
 
-    /**
-     * Initializes the array with words from the input file.
-     *
-     * @param fileName the name of the input file
-     * @return an array of words from the file
-     */
-    public static String[] array_Initialization(String fileName) throws FileNotFoundException {
-        int count = 0;
-
-        // Open the file for reading
+    public static char [] temp_Initialization (String fileName) throws FileNotFoundException {
         File myFile = new File(fileName);
         Scanner myInput = new Scanner(myFile);
 
-        // Count the number of words in the file
+        String temp = " ";
+
         while (myInput.hasNext()) {
-            myInput.next();
-            count++;
+            temp = myInput.nextLine();
         }
 
-        // Create an array of the appropriate size to store the words
-        String[] wordArr = new String[count];
         myInput.close();
 
-        // Fill the array with words from the file
-        return array_Fill(wordArr, myFile);
+        char [] dna_seq = new char [temp.length()];
+
+        int i = 0;
+        for (char c : temp.toCharArray()) {
+            dna_seq[i] = c;
+            i++;
+        }
+        return dna_seq;
+    }
+
+    public static String[] dna_convertTo_mRNA (char [] arr) {
+
+        String [] str = charToString(arr);
+
+        String [] mRNA = new String [arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            String temp = str[i];
+
+            switch (temp) {
+                case "A" -> mRNA[i] = "U";
+                case "T" -> mRNA[i] = "A";
+                case "C" -> mRNA[i] = "G";
+                case "G" -> mRNA[i] = "C";
+            }
+        }
+
+        return  mRNA;
     }
 
     /**
-     * Fills the word array with words from the input file
      *
-     * @param wordArr an array
-     * @param myFile  the input file containing the words
-     * @return the filled array
+     * @param arr
+     * @return
      */
-    public static String[] array_Fill(String[] wordArr, File myFile) throws FileNotFoundException {
-        Scanner myInput = new Scanner(myFile);
+    public static String [] mRNA_tripletConversion (String [] arr) {
 
-        // Fill the array with words from the file
-        int i = 0;
-        while (myInput.hasNext()) {
-            wordArr[i] = myInput.next();
-            i++;
+
+        int new_len = (arr.length)/(3);
+        String [] mRNA_triplet = new String [new_len];
+        int firstNuc = 0;
+        int thirdNuc = 2;
+        for (int i = 0; i < new_len; i++) {
+
+            String codon = "";
+
+            for (int j = firstNuc; j <= thirdNuc;j++) {
+
+                String temp = arr[j];
+
+                codon += temp;
+            }
+
+            mRNA_triplet[i] = codon;
+
+            firstNuc = firstNuc + 3;
+            thirdNuc = thirdNuc + 3;
+
         }
-        myInput.close();
 
-        return wordArr;
+        return  mRNA_triplet;
+
     }
+
+    /**
+     *
+     * @param arr
+     * @return
+     */
+    public static String[] mRNA_convertTo_Protein (String [] arr) {
+
+        String [] protein = new String [arr.length];
+
+        HashMap <String, String> codonChart = getCodonChart();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            String temp = arr[i];
+
+            if(getSTOP(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            }else if (getMET(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            }else if (getGLY(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            }else if (getALA(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            }else if (getSER(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            }else if (getTHR(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            }else if (getVAL(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            }else if (getPRO(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getLEU(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getARG(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getPHE(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getTYR(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getCYS(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getTRP(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getHIS(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getGIN(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getASN(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getLYS(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getASP(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            } else if (getGLU(arr[i])) {
+                protein[i] = codonChart.get(arr[i]);
+            }   else if (getILE(arr[i])) {
+                    protein[i] = codonChart.get(arr[i]);
+            }
+
+        }
+
+        return  protein ;
+    }
+
+    public static boolean getSTOP(String str) {
+        return str.equals("UAA") || str.equals("UAG") || str.equals("UGA");
+    }
+
+    public static boolean getMET(String str) {
+        return str.equals("AUG");
+    }
+
+    public static boolean getTRP (String str) {
+        return str.equals("UGG");
+    }
+
+    public static boolean getGLY(String str) {
+        return str.equals("GGU") || str.equals("GGC") || str.equals("GGA") || str.equals("GGG");
+    }
+
+    public static boolean getSER(String str) {
+        return str.equals("UCU") || str.equals("UCC") || str.equals("UCA") || str.equals("UCG") || str.equals("AGU") || str.equals("AGC");
+    }
+
+    public static boolean getALA(String str) {
+        return str.equals("GCU") || str.equals("GCC") || str.equals("GCA") || str.equals("GCG");
+    }
+
+    public static boolean getTHR(String str) {
+        return str.equals("ACU") || str.equals("ACC") || str.equals("ACA") || str.equals("ACG");
+    }
+
+    public static boolean getVAL(String str) {
+        return str.equals("GUU") || str.equals("GUC") || str.equals("GUA") || str.equals("GUG");
+    }
+
+    public static boolean getLEU(String str) {
+        return str.equals("CUU") || str.equals("CUC") || str.equals("CUA") || str.equals("CUG") || str.equals("UUA")|| str.equals("UUG");
+    }
+
+    public static boolean getPRO(String str) {
+        return str.equals("CCU") || str.equals("CCC") || str.equals("CCA") || str.equals("CCG");
+    }
+
+    public static boolean getARG(String str) {
+        return str.equals("CGU") || str.equals("CGC") || str.equals("CGA") || str.equals("CGG") || str.equals("AGA") || str.equals("AGG");
+    }
+
+    public static boolean getPHE(String str) {
+        return str.equals("UUU") || str.equals("UUC");
+    }
+
+    public static boolean getTYR(String str) {
+        return str.equals("UAU") || str.equals("UAC");
+    }
+
+    public static boolean getCYS (String str) {
+        return str.equals("UGU") || str.equals("UGC");
+    }
+
+    public static boolean getHIS (String str) {
+        return str.equals("CAU") || str.equals("CAC");
+    }
+
+    public static boolean getGIN (String str) {
+        return str.equals("CAA") || str.equals("CAG");
+    }
+
+    public static boolean getASN (String str) {
+        return str.equals("AAU") || str.equals("AAC");
+    }
+
+    public static boolean getLYS (String str) {
+        return str.equals("AAA") || str.equals("AAG");
+    }
+
+    public static boolean getASP (String str) {
+        return str.equals("GAU") || str.equals("GAC");
+    }
+
+    public static boolean getGLU (String str) {
+        return str.equals("GAA") || str.equals("GAG");
+    }
+
+    public static boolean getILE (String str) {
+        return str.equals("AUU") || str.equals("AUC")|| str.equals("AUA");
+    }
+
+
+
 
 }
